d0b947e343feec56102c841743920c9410100051
PaoloBaiada <paolosettemaggio@gmail.com>
2023-06-13 16:36:18 +0200

    crud

---
+++ exercise11/exercise11.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const {planets} = require('./planets.js')
+const app = express()
+app.use(express.json())
+
+app.get('/api/planets', (req,res) => {
+  res.status(200).json(planets)
+})
+
+app.get('/api/planets/:id',(req,res) => {
+  const {id} = req.params
+  const planet = planets.find((planet) => planet.id === Number(id))
+  res.json(planet)
+})
+
+app.post('/api/planets',(req,res) => {
+  const {id,name} = req.body
+  const newPlanet = {id,name}
+  planets = [...planets, newPlanet]
+
+  res.status(201).json({msg: 'the planet is added'})
+})
+
+app.put('/api/planets/:id',(req,res) => {
+const {id} = req.params
+const planet = req.body
+planets[Number(id) - 1] = planet
+res.status(200).json({msg: "value changed", data: planets})
+})
+
+app.delete('/api/planets/:id',(req,res) => {
+  const {id} = req.params
+  const index = planets.findIndex(planet => planet.id === id)
+  planets.splice(index,1)
+  res.status(200).json({msg: "deleted", data : planets})
+})
+
+app.listen(3000)
+
+ 
---
+++ exercise11/planets.js
@@ -0,0 +1,12 @@
+let planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+  ];
+
+module.exports=  {planets}
