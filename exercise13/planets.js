989ac3a9bb4f6245cc5d4c7bbdc29005897974fb
PaoloBaiada <paolosettemaggio@gmail.com>
2023-06-26 10:37:24 +0200

    update

--- exercise13/planets.js
+++ exercise13/planets.js
@@ -3,6 +3,8 @@ const pgPromise = require("pg-promise")();
 const db = pgPromise("postgres://postgres:postgres@localhost:5432/planets");
 const setUpDb = async () => {
   await db.none(`
+  DROP TABLE IF EXISTS planets;
+
     CREATE TABLE planets(
         id SERIAL PRIMARY KEY,
         name TEXT NOT NULL
@@ -15,57 +17,39 @@ const setUpDb = async () => {
 };
 setUpDb();
 
-let planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
-
 const getAll = async (req, res) => {
     const planets = await db.many(`SELECT * FROM planets;`)
   res.status(200).json(planets);
 };
 
-const getoneById = (req, res) => {
+const getoneById = async (req, res) => {
   const { id } = req.params;
-  const planet = planets.find((planet) => planet.id === Number(id));
+  const planet = await db.one(`SELECT * FROM planets WHERE id=$1;`, Number(id))
   res.json(planet);
 };
 
-const create = (req, res) => {
+const create = async (req, res) => {
   const { id, name } = req.body;
   const newPlanet = { id, name };
-  planets = [...planets, newPlanet];
+  await db.none(`INSERT INTO planets (name) VALUES $1`, name)
 
   res.status(201).json({ msg: "the planet is added", data: planets });
 };
 
 const updateById = (req, res) => {
   const { id } = req.params;
-  const update = req.body;
-  planets = planets.map((planet) => {
-    if (planet.id === Number(id)) {
-      return { ...planet, ...update };
-    }
-    return planet;
-  });
+  const {name} = req.body;
+ db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id,name])
   res.status(200).json({ msg: "value changed", data: planets });
 };
-
-const deleteById = (req, res) => {
+const deleteById = async (req, res) => {
   const { id } = req.params;
-  planets = planets.filter((planet) => planet.id !== Number(id));
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(id))
 
   res.status(200).json({ msg: "deleted", data: planets });
 };
 
 module.exports = {
-  planets,
   getAll,
   getoneById,
   create,
